fae96a079ed19dd074f1b25bca6d7cedaa4105ca
David Wu <david204code@gmail.com>
2020-04-24 00:06:29 +0100

    Add the checkLoginStatus function

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -17,12 +17,39 @@ class App extends React.Component {
     this.handleLogin = this.handleLogin.bind(this);
   }
 
+  checkLoginStatus() {
+    axios
+    .get("http://localhost:3000/logged_in", { withCredentials: true})
+    .then(response => {
+      if (response.data.logged_in && this.state.loggedInStatus === "NOT_LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "LOGGED_IN",
+          user: response.data.user
+        })
+      } else if (!response.data.logged_in && this.state.loggedInStatus === "LOGGED_IN") {
+        this.setState({
+          loggedInStatus: "NOT_LOGGED_IN",
+          user: {}
+        })
+      }
+    })
+    .catch(error => {
+      console.log("check login error", error);
+    });
+  };
+  
+  componentDidMount() {
+    this.checkLoginStatus();
+  }
+  
+
   handleLogin(data) {
     this.setState({
       loggedInStatus: "Logged_In",
       user: data.user
     });
-  }
+  };
+  
 
   render() {
     return (
