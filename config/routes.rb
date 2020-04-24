8bd018f61ba5155cc3f2feb804db0ec76c80f5f4
David Wu <david204code@gmail.com>
2020-04-24 19:02:41 +0100

    add resouces: helps, to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :registrations, only: [:create]
   delete :logout, to: "sessions#logout"
   get :logged_in, to: "sessions#logged_in"
+
+  resources :helps
+
   root 'pages#home'
   get 'pages/home'
   get 'pages/dashboard'
