33606726cd24e655c776842c6d29dc8c07ca11ff
David Wu <david204code@gmail.com>
2020-04-24 18:35:42 +0100

    Tidy up the Home and Login Component

--- app/javascript/components/pages/Home.jsx
+++ app/javascript/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import SignUp from '../auth/SignUp';
 import Login from '../auth/Login';
 import axios from 'axios';
@@ -30,12 +31,50 @@ class Home extends React.Component {
 
   render() {
     return (
-    <div>
+    <div className = "container">
       <h1>Home</h1>
       <h1>Status: {this.props.loggedInStatus}</h1>
       <button onClick={() => this.handleLogOutClick()}>Logout</button>
-      <SignUp handleSuccessfulAuth ={this.handleSuccessfulAuth}/>
-      <Login handleSuccessfulAuth ={this.handleSuccessfulAuth} />
+      <div className ="row">
+        <div className ="col-md-5 text-center" style={{backgroundColor: "lightblue"}}>
+          <p className ="pt-4">
+            We are connecting people and buidling a community in a time of need and 
+            also in the time of goodness. Whether you want to provide generous help or 
+            recieve help in time of difficulties....
+          </p>
+        </div>
+        <div className ="col-md-5 text-center pt-4">
+          <h5>Get involve now!</h5>
+          <SignUp handleSuccessfulAuth={this.handleSuccessfulAuth} />
+        </div>
+      </div>
+
+      <div className ="">
+        <h3 className ="">Quick Guide</h3>
+        <p>
+          Sign up with an email address, password and a copy of government-approved ID (.jpg, .pgn, .pdf)
+        </p>
+        <p>
+          Broswer the map for those who are requesting help.
+        </p>
+        <p>
+          Need help yourself? Make your own request.
+        </p>
+      </div>
+
+      <div className ="row mb-3">
+        <div className ="col-md-6 offset-md-3">
+          <Login handleSuccessfulAuth ={this.handleSuccessfulAuth}/>
+        </div>
+      </div>
+
+      <div className ="">
+        <h3 className ="">Contact Us</h3>
+        <p>
+
+        </p>
+      </div>
+
     </div>
     );
   };
