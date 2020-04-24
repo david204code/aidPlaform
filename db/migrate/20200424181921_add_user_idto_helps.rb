bbb4ce16af5dd614e96345e787661028465e7b40
David Wu <david204code@gmail.com>
2020-04-24 19:23:52 +0100

    rails generate migration AddUserIdtoHelps user:references, config its migratio file, rails db:migrate, config User/Help model

---
+++ db/migrate/20200424181921_add_user_idto_helps.rb
@@ -0,0 +1,5 @@
+class AddUserIdtoHelps < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :helps, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_175303) do
+ActiveRecord::Schema.define(version: 2020_04_24_181921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_175303) do
     t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_helps_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,5 @@ ActiveRecord::Schema.define(version: 2020_04_24_175303) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "helps", "users"
 end
