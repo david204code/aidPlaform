3e936bd15757a93bef9b3bbc77a1e6a9ce955d89
David Wu <david204code@gmail.com>
2020-04-27 00:27:50 +0100

    Redid the actions of the SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,42 +1,49 @@
 class SessionsController < ApplicationController
-  include CurrentUserConcern
+  # include CurrentUserConcern
 
-  #post request
   def create
-    user = User
-    .find_by(email: params["user"]["email"])
-    .try(:authenticate, params["user"]["password"])
+    @user = User.find_by(email: session_params[:email])
 
-    if user 
-      session[:user_id] = user.id
+    if @user && @user.authenticate(session_params[:password])
+      login!
       render json: {
-        status: :created,
         logged_in: true,
-        user: user
-      }
-    else
-      render json: { status: 401 }
-    end
-  end
-
-  #get request
-  def logged_in
-    if @current_user
-      render json: { 
-        logged_in: true,
-        user: @current_user
+        user: @user
       }
     else
       render json: {
-        logged_in: false
+        status: 401,
+        errors: ['no such user', 'verify credentials and try again or signup']
       }
     end
   end
 
-  #delete request
-  def logout
-    reset_session
-    render json: { status: 200, logged_out: true }
+  def is_logged_in?
+    if logged_in? && current_user
+      render json: {
+        logged_in: true,
+        user: current_user
+      }
+    else
+      render json: {
+        logged_in: false,
+        message: 'no such user'
+      }
+    end
+  end
+
+  def destroy 
+    logout!
+    render json: {
+      status: 200,
+      logged_out: true
+    }
+  end
+
+  private
+
+  def session_params
+    params.require(:user).permit(:email, :password)
   end
 
 end
