cbe6df4abf4ea9c4d36ad92e4a2cbc21e62e826a
David Wu <david204code@gmail.com>
2020-07-07 00:44:26 +0100

    Starting the Messaging function in the rails by removing the foreign key

--- db/migrate/20200527140735_add_user_id_and_help_id_to_messages.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdAndHelpIdToMessages < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :messages, :user, foreign_key: true
-    add_reference :messages, :help, foreign_key: true
-  end
-end
--- db/migrate/20200601220148_add_accepted_help_id_to_messages.rb
+++
@@ -1,5 +0,0 @@
-class AddAcceptedHelpIdToMessages < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :messages, :accepted_help, foreign_key: true
-  end
-end
